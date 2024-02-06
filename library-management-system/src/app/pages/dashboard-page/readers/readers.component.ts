4c4affd767bc42fa01dc06e80f6aab888ef0c6c8
Jithma Wickramarachchi <151661874+ProphetAlphaX@users.noreply.github.com>
2024-02-06 21:17:42 +0530

    Set readers data into table and view reader details modal done

--- library-management-system/src/app/pages/dashboard-page/readers/readers.component.ts
+++ library-management-system/src/app/pages/dashboard-page/readers/readers.component.ts
@@ -1,11 +1,13 @@
+import { CommonModule, NgFor } from '@angular/common';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-readers',
   standalone: true,
-  imports: [HttpClientModule, RouterLink],
+  imports: [HttpClientModule, RouterLink, FormsModule, CommonModule, NgFor],
   templateUrl: './readers.component.html',
   styleUrl: './readers.component.css'
 })
@@ -13,6 +15,7 @@ export class ReadersComponent {
 
   private http;
   public readersList: any;
+  public recentSavedReader : any;
 
   constructor(httpClient: HttpClient) {
     this.http = httpClient;
@@ -27,6 +30,10 @@ export class ReadersComponent {
       this.readersList = data;
       console.log(this.readersList)
     })
+  }
+
+  saveReader(reader : any){
+    this.recentSavedReader = reader;
   }
 
 }
