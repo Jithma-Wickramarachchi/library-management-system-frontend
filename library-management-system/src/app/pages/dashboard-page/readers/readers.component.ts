9a9fe19539e8ec66be8ad4498e15c8da2579ae23
Jithma Wickramarachchi <151661874+ProphetAlphaX@users.noreply.github.com>
2024-02-06 17:47:28 +0530

    Get readers list from server to the client setup

--- library-management-system/src/app/pages/dashboard-page/readers/readers.component.ts
+++ library-management-system/src/app/pages/dashboard-page/readers/readers.component.ts
@@ -1,13 +1,32 @@
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-readers',
   standalone: true,
-  imports: [RouterLink],
+  imports: [HttpClientModule, RouterLink],
   templateUrl: './readers.component.html',
   styleUrl: './readers.component.css'
 })
 export class ReadersComponent {
+
+  private http;
+  public readersList: any;
+
+  constructor(httpClient: HttpClient) {
+    this.http = httpClient;
+  }
+
+  ngOnInit():void {
+    this.loadReaders();
+  }
+
+  loadReaders() {
+    this.http.get('http://localhost:8080/reader/list').subscribe((data) => {
+      this.readersList = data;
+      console.log(this.readersList)
+    })
+  }
 
 }
