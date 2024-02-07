b0d29c8b0f684ca91c8e723c8bbfaca2b4854d6b
Jithma Wickramarachchi <151661874+ProphetAlphaX@users.noreply.github.com>
2024-02-07 11:40:50 +0530

    Showing " successfull modal" when a reader added done

--- library-management-system/src/app/pages/dashboard-page/readers/readers.component.ts
+++ library-management-system/src/app/pages/dashboard-page/readers/readers.component.ts
@@ -1,8 +1,10 @@
 import { CommonModule, NgFor } from '@angular/common';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
-import { Component } from '@angular/core';
+import { APP_BOOTSTRAP_LISTENER, Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { bootstrapApplication } from '@angular/platform-browser';
 import { RouterLink } from '@angular/router';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-readers',
@@ -35,6 +37,14 @@ export class ReadersComponent {
   saveReader(reader: any) {
     this.recentSavedReader = reader;
   }
+  clearNewReader(){
+    this.newReader = {
+      name: null,
+      nic: null,
+      contact: null,
+      address: null
+    };
+  }
 
   loadReaders() {
     this.http.get('http://localhost:8080/reader/list').subscribe((data) => {
@@ -51,6 +61,12 @@ export class ReadersComponent {
   addReader() {
     this.http.post('http://localhost:8080/reader/add', this.newReader).subscribe((data) => {
       console.log(data);
+      Swal.fire({
+        title: "Successfull!",
+        text: "Reader \'"+this.newReader.name+"\' added successfully",
+        icon: "success"
+      });
+      this.clearNewReader();
       this.ngOnInit();
     })
   }
