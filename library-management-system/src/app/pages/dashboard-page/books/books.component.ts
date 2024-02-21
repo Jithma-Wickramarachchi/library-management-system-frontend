b61be7b42a1725449b3a7cea2b468f915b4bfe93
Jithma Wickramarachchi <151661874+ProphetAlphaX@users.noreply.github.com>
2024-02-21 22:06:23 +0530

    show book list on the table done

--- library-management-system/src/app/pages/dashboard-page/books/books.component.ts
+++ library-management-system/src/app/pages/dashboard-page/books/books.component.ts
@@ -1,13 +1,33 @@
+import { NgFor } from '@angular/common';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-books',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, HttpClientModule, NgFor],
   templateUrl: './books.component.html',
   styleUrl: './books.component.css'
 })
 export class BooksComponent {
+  private http;
+  bookList: any;
+
+  constructor(httpClient: HttpClient) {
+    this.http = httpClient;
+  }
+
+  ngOnInit() {
+    this.loadTable();
+  }
+
+  loadTable() {
+    this.http.get('http://localhost:8080/book').subscribe((data) => {
+      console.log(data)
+      this.bookList = data;
+    })
+
+  }
 
 }
