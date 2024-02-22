114484a6cfb7168a57690deb76857c2fccb3d816
Jithma Wickramarachchi <151661874+ProphetAlphaX@users.noreply.github.com>
2024-02-22 08:30:33 +0530

    Add a book to database throught frontend done

--- library-management-system/src/app/pages/dashboard-page/books/books.component.ts
+++ library-management-system/src/app/pages/dashboard-page/books/books.component.ts
@@ -1,18 +1,26 @@
-import { NgFor } from '@angular/common';
+import { CommonModule, NgFor } from '@angular/common';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-books',
   standalone: true,
-  imports: [RouterLink, HttpClientModule, NgFor],
+  imports: [RouterLink, HttpClientModule, NgFor, CommonModule, FormsModule],
   templateUrl: './books.component.html',
   styleUrl: './books.component.css'
 })
 export class BooksComponent {
   private http;
   bookList: any;
+  newBook = {
+    isbn : null,
+    title : null,
+    author : null,
+    category : null,
+    status : null
+  };
 
   constructor(httpClient: HttpClient) {
     this.http = httpClient;
@@ -22,12 +30,27 @@ export class BooksComponent {
     this.loadTable();
   }
 
+  clearNewBook(){
+    this.newBook = {
+      isbn : null,
+      title : null,
+      author : null,
+      category : null,
+      status : null
+    };
+  }
   loadTable() {
     this.http.get('http://localhost:8080/book').subscribe((data) => {
       console.log(data)
       this.bookList = data;
     })
-
+  }
+  addBook(){
+    this.http.post('http://localhost:8080/book',this.newBook).subscribe((data)=>{
+      console.log(data);
+      this.ngOnInit();
+      this.clearNewBook();
+    })
   }
 
 }
