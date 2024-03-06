4d8d59eb6e3db143932c3f56812801098aeaa3ca
Jithma Wickramarachchi <151661874+ProphetAlphaX@users.noreply.github.com>
2024-03-06 12:27:30 +0530

    Updated UI for clean view

--- library-management-system/src/app/pages/dashboard-page/dashboard-page.component.ts
+++ library-management-system/src/app/pages/dashboard-page/dashboard-page.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterModule } from '@angular/router';
-import { SidebarComponent } from './common/sidebar/sidebar.component';
 import { ReadersComponent } from './readers/readers.component';
+import { NavbarComponent } from './common/navbar/navbar.component';
 
 @Component({
   selector: 'app-dashboard-page',
   standalone: true,
-  imports: [RouterLink, SidebarComponent, ReadersComponent, RouterModule],
+  imports: [RouterLink, NavbarComponent, ReadersComponent, RouterModule],
   templateUrl: './dashboard-page.component.html',
   styleUrl: './dashboard-page.component.css'
 })
